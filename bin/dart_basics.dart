32882a19c7d45591be1854bb56aea2f93a8175a1
RavenP <ravenp.dev@gmail.com>
2023-06-15 10:37:34 +0800

    variables

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,3 +1,5 @@
+import 'package:dart_basics/variables.dart' as variables;
+
 void main(List<String> arguments) {
-  //
+  variables.variables();
 }
---
+++ lib/variables.dart
@@ -0,0 +1,15 @@
+void variables() {
+  String firstName = 'raven';
+  String lastName = 'paragas';
+  int age = 30;
+  double randomNumber = 3.3;
+
+  print('My name is $firstName $lastName and I am $age years old.');
+  print('Random number: $randomNumber');
+
+  // declaration
+  late int lateInt;
+  // allocation
+  lateInt = 42;
+  print('lateInt: $lateInt');
+}
